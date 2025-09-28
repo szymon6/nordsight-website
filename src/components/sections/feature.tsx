b05cee3ace625f647be1dc98796fe4194e0837e6
szymon6 <72827230+szymon6@users.noreply.github.com>
2025-09-28 08:48:24 +0200

    c

--- src/components/sections/feature.tsx
+++ src/components/sections/feature.tsx
@@ -1,7 +1,7 @@
 import { Feature } from '@/components/ui/feature-section-with-bento-grid'
 export function FeatureSection() {
   return (
-    <div className="relative z-10 w-full pt-20">
+    <div className="relative z-10 w-full pt-10">
       <Feature />
     </div>
   )
